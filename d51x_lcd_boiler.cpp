fa49ab124ff0f182bde78124bad796c19df50767
d51x <dimonich110@gmail.com>
2017-03-10 01:31:49 +0300

    сделал отображения своей информации на LCD

--- d51x_lcd_boiler.cpp
+++ d51x_lcd_boiler.cpp
@@ -1,79 +1,99 @@
-static os_timer_t esp_timer; // глобально объявим таймер esp_timer
-
-const char str_boiler = "Boiler ";
-const char str_pump = "Pump   ";
-const char str_hotcab = "Hotcab ";
-const char str_power = " Power ";
-const char str_airt = "AirT:%s%02d.%01d*C H:%02d.%01d\%";
-const char str_current_power = " %02d.%01dA  %04dW"; // " 10.6A  1534W"
-const char str_max_power_load_date = "%04d %02d.%02d.%02d"; // "1534 22.12.17"
-const char str_max_power_load_time = "%04d %02d:%02d:"; // "1534 13:12"
-const char str_max_current_load_date = "%02d.%01d %02d.%02d.%02d"; // "15.4 22.12.17"
-const char str_max_current_load_time = "%02d.%01d %02d:%02d:"; // "15.4 13:12"
-const char str_page_header_2 = "***  Power Load  ***";
-const char str_page_header_3 = "***** MAX Power	****";
-const char str_page_header_4 = "**** MAX Current ***";
-
-#define LCD_UPDATE_TIME = 2 // sec
+#define LCD_UPDATE_TIME  2 // sec
 #define GPIO_BOILER 16
 #define GPIO_PUMP 15
 #define GPIO_HOTCAB 14
 
-// запуск таймера:
-os_timer_disarm(&esp_timer);
-os_timer_setfn(&esp_timer, (os_timer_func_t *)read_esp, NULL); // read_esp -функцию, которую нужно вызвать по таймеру.
-os_timer_arm(&esp_timer, 1000, 1); // 1000 миллисекунд. 1 - многократно. 0 -однократно.
+#define ADC_PAGE 820
 
-void ICACHE_FLASH_ATTR read_esp(){
-	// тут пишем код, который будет вызываться по таймеру
+#define page1_line1 "Boiler %s %6d.%1dВ°"
+#define page1_line2 "Pump   %s Power %s"
+#define page1_line3 "Hotcab %s %6d.%1dВ°"
+#define page1_line4 "Hum %2d.%1d%%  Air %2d.%1dВ°"
+
+#define str_current_power " %02d.%01dA  %04dW" // " 10.6A  1534W"
+#define str_max_power_load_date "%04d %02d.%02d.%02d" // "1534 22.12.17"
+#define str_max_power_load_time "%04d %02d:%02d:" // "1534 13:12"
+#define str_max_current_load_date "%02d.%01d %02d.%02d.%02d" // "15.4 22.12.17"
+#define str_max_current_load_time "%02d.%01d %02d:%02d:" // "15.4 13:12"
+#define str_page_header_2 "***  Power Load  ***"
+#define str_page_header_3 "***** MAX Power	****"
+#define str_page_header_4 "**** MAX Current ***"
+
+#define PAGES_COUNT 4
+uint8_t page = 0;
+
+void update_LCD() {
 	uint8_t   lcd_line = 0;
 	char lcd_line_text[40] = "";
+	int16_t temp = 0;
 	
-	memset(lcd_line_text, 0, 40);
-	// ***************** page 1, line 1 ******************************
-	lcd_line = 0;
-	//boiler state
-	strcpy(lcd_line_text, str_boiler);
-	strcat(lcd_line_text, (GPIO_ALL_GET(GPIO_BOILER) == 1) ? "ON " : "OFF");
-	strcat(lcd_line_text, " T:");
-	int32_t t = data1wire[0];  // выц1
-	if ( t < 0 ) {
-		strcat(lcd_line_text, "+%02d.%01d*C");
-	} else if ( t > 0 ) {
-		strcat(lcd_line_text, "+%02d.%01d*C");
-	} else {
-		strcat(lcd_line_text, " %02d.%01d*C");
-	}
-	os_sprintf(lcd_line_text, (int)(t / 10), (int)(t % 10));
-	LCD_print(lcd_line, lcd_line_text);
+/* 	if ( (analogRead() > (ADC_PAGE - 10)) && (analogRead() < (ADC_PAGE + 10)) ) {
+		page++;
+		if (page > PAGES_COUNT - 1) page = 0;
+	} */
 	
+	switch (page) {
+		case 0: 
+			// ***************** page 1, line 1 ******************************
+			os_memset(lcd_line_text, 0, 40);	
+			lcd_line = 0;
+			os_sprintf(lcd_line_text, page1_line2, (GPIO_ALL_GET(GPIO_PUMP) == 1) ? "ON " : "OFF", (GPIO_ALL_GET(GPIO_PUMP) == 1) ? "ON " : "OFF");
+			LCD_print(lcd_line, lcd_line_text);	
 	
-	// ***************** page 1, line 2 ******************************
-	memset(lcd_line_text, 0, 40);
-	lcd_line = 1;
-	//pump state + power state
-	strcpy(lcd_line_text, str_pump);
-	strcat(lcd_line_text, (GPIO_ALL_GET(GPIO_PUMP) == 1) ? "ON " : "OFF");
-	strcat(lcd_line_text, str_power);
-	// get data from ADC 1
+			// ***************** page 1, line 2 ******************************
+			os_memset(lcd_line_text, 0, 40);	
+			lcd_line = 1;
+			temp = data1wire[0];  // РІС‹С†1
+			os_sprintf(lcd_line_text, page1_line1, (GPIO_ALL_GET(GPIO_BOILER) == 1) ? "ON " : "OFF", (int)(temp / 10), (int)(temp % 10));
+			LCD_print(lcd_line, lcd_line_text);		
 	
-	LCD_print(lcd_line, lcd_line_text);	
+
+			// ***************** page 1, line 3 ******************************
+			os_memset(lcd_line_text, 0, 40);	
+			lcd_line = 2;
+			temp = data1wire[1];  // 
+			os_sprintf(lcd_line_text, page1_line3, (GPIO_ALL_GET(GPIO_HOTCAB) == 1) ? "ON " : "OFF", (int)(temp / 10), (int)(temp % 10));
+			LCD_print(lcd_line, lcd_line_text);		
+			
+			// ***************** page 1, line 4 ******************************
+			os_memset(lcd_line_text, 0, 40);	
+			lcd_line = 3;
+			temp = dht_t1;
+			uint16_t hum = dht_h1;
+			os_sprintf(lcd_line_text, page1_line4, (int)(temp / 10), (int)(temp % 10), (int)(hum / 10), (int)(hum % 10));
+			LCD_print(lcd_line, lcd_line_text);	
+			break;	
+		case 1:
+			os_memset(lcd_line_text, 0, 40);	
+			LCD_print(2, lcd_line_text);	
+			LCD_print(3, lcd_line_text);	
+			break;
+		case 2:
+			os_memset(lcd_line_text, 0, 40);	
+			LCD_print(0, lcd_line_text);	
+			LCD_print(1, lcd_line_text);	
+			break;			
+	}	
+			
 }
 
 
-void ICACHE_FLASH_ATTR startfunc(){
-	// выполняется один раз при старте модуля.
+void ICACHE_FLASH_ATTR
+startfunc(){
+// РІС‹РїРѕР»РЅСЏРµС‚СЃСЏ РѕРґРёРЅ СЂР°Р· РїСЂРё СЃС‚Р°СЂС‚Рµ РјРѕРґСѓР»СЏ.
 }
 
-void ICACHE_FLASH_ATTR timerfunc(uint32_t  timersrc) {
-	// место для вставки кода, который будет выполнятся каждую 1 секунду.
+void ICACHE_FLASH_ATTR
+ timerfunc(uint32_t  timersrc) {
+// РІС‹РїРѕР»РЅРµРЅРёРµ РєРѕРґР° РєР°Р¶РґСѓСЋ 1 СЃРµРєСѓРЅРґСѓ
 
-	if(timersrc%30==0){
-		// место для вставки кода, который будет выполнятся каждые 30 секунд.
-		
-	}
+ update_LCD();
+
+if(timersrc%30==0){
+// РІС‹РїРѕР»РЅРµРЅРёРµ РєРѕРґР° РєР°Р¶РґС‹Рµ 30 СЃРµРєСѓРЅРґ
+}
 }
 
 void webfunc(char *pbuf) {
-	os_sprintf(HTTPBUFF,"Hello world"); // вывод данных на главной модуля
+os_sprintf(HTTPBUFF,"<br>analogRead() = %d", analogRead()); // РІС‹РІРѕРґ РґР°РЅРЅС‹С… РЅР° РіР»Р°РІРЅРѕР№ РјРѕРґСѓР»СЏ
 }
