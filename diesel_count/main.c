d942adc299fc3272fd22f5942057c3a574ce9cdc
d51x <dimonich110@gmail.com>
2021-12-30 15:03:29 +0300

    diesel: переделал сохранение данных

--- diesel_count/main.c
+++ diesel_count/main.c
@@ -1,5 +1,6 @@
+#define FW_VER "1.4"
 /*
- Отсечка счетчика, Время работы общее, Время работы сегодня, Расход общий, Расход сегодня,Время работы вчера,Расход вчера
+Отсечка счетчика,Время общее,Расход общий,Время предыдущее,Расход предыдущий,Время сегодня,Расход сегодня,Время вчера,Расход вчера
  
 if(gpioget(15)==1)
 gpioset(12,1)
@@ -18,17 +19,11 @@ endif
 
 #define millis() (uint32_t) (micros() / 1000ULL) 
 
-#define FUEL_PUMP "fuelpump_state"
+#define FUEL_PUMP "fPumpState"
 
-// Отсечка счетчика, Время работы общее, Время работы сегодня, Расход общий, Расход сегодня,Время работы вчера,Расход вчера
-
-#define COUNTER_THRESHOLD sensors_param.cfgdes[0]
-#define SAVE_FUEL_WORKTIME_TOTAL sensors_param.cfgdes[1]
-#define SAVE_FUEL_WORKTIME_DAY sensors_param.cfgdes[2]
-#define SAVE_FUEL_CONSUMPTION_TOTAL sensors_param.cfgdes[3]
-#define SAVE_FUEL_CONSUMPTION_DAY sensors_param.cfgdes[4]
-#define SAVE_FUEL_WORKTIME_PREV sensors_param.cfgdes[5]
-#define SAVE_FUEL_CONSUMPTION_PREV sensors_param.cfgdes[6]
+#define SENS sensors_param
+#define SENSCFG sensors_param.cfgdes
+#define COUNTER_THRESHOLD SENSCFG[0]
 
 //#define ADDLISTSENS {201,LSENSFL0,"имя","val1",&cnttest,NULL}, uint16_t cnttest;
 // можно так уже к существущей переменной
@@ -44,45 +39,66 @@ endif
 #define CONSUMP_ML_SEC 0.55f
 #define CONSUMP_L_SEC 100000*0.00055f
 
-uint16_t fuel_pump_state = 0;
-uint32_t fuel_pump_working_time = 0;
-uint32_t fuel_pump_today_time = 0;
-uint32_t fuel_pump_prev_time = 0;
+uint16_t fpState=0;
 
-uint32_t get_consump();
-uint32_t i_fuel_consump;
-uint32_t i_fuel_consump_now;
+//uint32_t fpWorkA=0;		// время работы общее
+#define fpWorkA SENSCFG[1]
+//uint32_t fpWorkT=0;		// время работы сегодня
+#define  fpWorkT SENSCFG[5]
+//uint32_t fpWorkY=0;		// время работы вчера
+#define fpWorkY SENSCFG[7]
+//uint32_t fpWorkL=0;		// время работы предыдущее
+#define fpWorkL SENSCFG[3]
+uint32_t fpWorkN=0; 	// время работы текущее
 
-uint32_t get_consump_today();
-uint32_t i_fuel_consump_today;
+//uint32_t fpRateA=0;		// расход общее
+#define fpRateA SENSCFG[2]
+//uint32_t fpRateT=0;		// расход сегодня
+#define fpRateT SENSCFG[6]
+//uint32_t fpRateY=0;		// расход вчера
+#define fpRateY SENSCFG[8]
+//uint32_t fpRateL=0;		// расход предыдущее
+#define fpRateL SENSCFG[4]
+uint32_t fpRateN=0; 	// расход текущее
 
-uint32_t get_consump_prev();
-uint32_t i_fuel_consump_prev;
+uint32_t getFuelRateTotal() {return 	fpRateA / 100;}
+uint32_t getFuelRateToday() {return 	fpRateT / 100;}
+uint32_t getFuelRatePrev()	{return 	fpRateY / 100;}
+uint32_t getFuelRateLast()	{return 	fpRateL / 100;}
+uint32_t getFuelRateNow()	{return 	fpRateN / 100;}
+
+uint32_t getFuelWorkTotal() {return 	fpWorkA;}
+uint32_t getFuelWorkToday() {return 	fpWorkT;}
+uint32_t getFuelWorkPrev()	{return 	fpWorkY;}
+uint32_t getFuelWorkLast()	{return 	fpWorkL;}
+
 
 uint16_t on_cnt = 0;
-uint32_t on_duration = 0;
-uint32_t prev_duration = 0;
 
-#define ADDLISTSENS {200,LSENSFL0,"FuelPump",FUEL_PUMP,&fuel_pump_state,NULL}, \
-					{201,LSENSFL3|LSENS32BIT|LSENSFUNS,"FuelConsumpTotal","fuelcons_total",get_consump,NULL}, \
-					{202,LSENSFL3|LSENS32BIT|LSENSFUNS,"FuelConsumpToday","fuelcons_day",get_consump_today,NULL}, \
-					{203,LSENSFL3|LSENS32BIT|LSENSFUNS,"FuelConsumpPrev","fuelcons_prev",get_consump_prev,NULL}, \
-					{204,LSENSFL0|LSENS32BIT,"WorkTimeTotal","fuelpump_total",&fuel_pump_working_time,NULL}, \
-					{205,LSENSFL0|LSENS32BIT,"WorkTimeDay","fuelpump_day",&fuel_pump_today_time,NULL}, \
-					{206,LSENSFL0|LSENS32BIT,"WorkTimePrev","fuelpump_prev",&fuel_pump_prev_time,NULL}, \
-					{207,LSENSFL0,"OnCount","oncnt",&on_cnt,NULL}, \
-					{208,LSENSFL0|LSENS32BIT,"OnDuration","ondur",&prev_duration,NULL}, 
-
-
+#define ADDLISTSENS {200,LSENSFL0,"FuelPump",FUEL_PUMP,&fpState,NULL}, \
+					{201,LSENSFL3|LSENS32BIT|LSENSFUNS,"fuelRateTotal","fRateA",getFuelRateTotal,NULL}, \
+					{202,LSENSFL3|LSENS32BIT|LSENSFUNS,"fuelRateToday","fRateT",getFuelRateToday,NULL}, \
+					{203,LSENSFL3|LSENS32BIT|LSENSFUNS,"fuelRatePrev","fRateY",getFuelRatePrev,NULL}, \
+					{204,LSENSFL3|LSENS32BIT|LSENSFUNS,"fuelRateLast","fRateL",getFuelRateLast,NULL}, \
+					{205,LSENSFL3|LSENS32BIT|LSENSFUNS,"fuelRateNow","fRateN",getFuelRateNow,NULL}, \
+					{206,LSENSFL0|LSENS32BIT|LSENSFUNS,"fuelWorkTotal","fpWorkA",getFuelWorkTotal,NULL}, \
+					{207,LSENSFL0|LSENS32BIT|LSENSFUNS,"fuelWorkToday","fpWorkT",getFuelWorkToday,NULL}, \
+					{208,LSENSFL0|LSENS32BIT|LSENSFUNS,"fuelWorkPrev","fpWorkY",getFuelWorkPrev,NULL}, \
+					{209,LSENSFL0|LSENS32BIT|LSENSFUNS,"fuelWorkLast","fpWorkL",getFuelWorkLast,NULL}, \
+					{210,LSENSFL0|LSENS32BIT,"fuelWorkNow","fpWorkN",&fpWorkN,NULL}, \
+					{211,LSENSFL0,"OnCount","oncnt",&on_cnt,NULL},
 	typedef struct {
-		uint32_t fuel_consump_total;
-		uint32_t fuel_consump_day;
-		uint32_t fuel_consump_prev;
-		uint32_t fuel_worktime_total;
-		uint32_t fuel_worktime_day;
-		uint32_t fuel_worktime_prev;
+		uint32_t _fpRateA;
+		uint32_t _fpRateT;
+		uint32_t _fpRateY;
+		uint32_t _fpRateL;
+
+		uint32_t _fpWorkA;
+		uint32_t _fpWorkT;
+		uint32_t _fpWorkY;
+		uint32_t _fpWorkL;
+
 		uint16_t on_cnt;
-		uint32_t prev_duration;
 		uint32_t crc32;
 	} rtc_data_t;
 
@@ -90,20 +106,7 @@ uint32_t prev_duration = 0;
 	
 	uint8_t opt_saving = 0;
 	
-uint32_t get_consump()
-{
-	return 	i_fuel_consump / 100;
-}
 
-uint32_t get_consump_today()
-{
-	return 	i_fuel_consump_today / 100;
-}
-
-uint32_t get_consump_prev()
-{
-	return 	i_fuel_consump_prev / 100;
-}
 
 uint32_t calcCRC32(const uint8_t *data, uint16_t sz) {
   // Обрабатываем все данные, кроме последних четырёх байтов,
@@ -142,26 +145,18 @@ void ICACHE_FLASH_ATTR startfunc()
 		system_rtc_mem_write(70, &rtc_data, sizeof(rtc_data_t));		
 	}
 	
-	i_fuel_consump = rtc_data.fuel_consump_total;
-	i_fuel_consump_today = rtc_data.fuel_consump_day;
-	i_fuel_consump_prev = rtc_data.fuel_consump_prev;
-	
-	fuel_pump_working_time = rtc_data.fuel_worktime_total;
-	fuel_pump_today_time = rtc_data.fuel_worktime_day;
-	fuel_pump_prev_time = rtc_data.fuel_worktime_prev;
-	
 	on_cnt = rtc_data.on_cnt;
-	prev_duration = rtc_data.prev_duration;
 	
-	// если значения данных из rtс меньше чем сохраненные данные
-	if ( fuel_pump_working_time < SAVE_FUEL_WORKTIME_TOTAL ) fuel_pump_working_time = SAVE_FUEL_WORKTIME_TOTAL;
-	if ( fuel_pump_today_time < SAVE_FUEL_WORKTIME_DAY )	 fuel_pump_today_time = SAVE_FUEL_WORKTIME_DAY;
-	if ( fuel_pump_prev_time < SAVE_FUEL_WORKTIME_PREV )	 fuel_pump_prev_time = SAVE_FUEL_WORKTIME_PREV;
+	// если значения сохраненных данных меньше чем данные из rtc
+	if (fpWorkA < rtc_data._fpWorkA) fpWorkA = rtc_data._fpWorkA;
+	if (fpWorkT < rtc_data._fpWorkT) fpWorkT = rtc_data._fpWorkT;
+	if (fpWorkY < rtc_data._fpWorkY) fpWorkY = rtc_data._fpWorkY;
+	if (fpWorkL < rtc_data._fpWorkL) fpWorkL = rtc_data._fpWorkL;
 	
-	if ( i_fuel_consump < SAVE_FUEL_CONSUMPTION_TOTAL )		 i_fuel_consump = SAVE_FUEL_CONSUMPTION_TOTAL;
-	if ( i_fuel_consump_today < SAVE_FUEL_CONSUMPTION_DAY )  i_fuel_consump_today = SAVE_FUEL_CONSUMPTION_DAY;
-	if ( i_fuel_consump_prev < SAVE_FUEL_CONSUMPTION_PREV )  i_fuel_consump_prev = SAVE_FUEL_CONSUMPTION_PREV;
-
+	if (fpRateA < rtc_data._fpRateA) fpRateA = rtc_data._fpRateA;
+	if (fpRateT < rtc_data._fpRateT) fpRateT = rtc_data._fpRateT;
+	if (fpRateY < rtc_data._fpRateY) fpRateY = rtc_data._fpRateY;
+	if (fpRateL < rtc_data._fpRateL) fpRateL = rtc_data._fpRateL;
 }
 
 
@@ -176,210 +171,184 @@ static void ICACHE_FLASH_ATTR mqtt_send_int(const char *topic, int val)
 }
 
 
-void ICACHE_FLASH_ATTR save_data(uint8_t save)
-{
-	SAVE_FUEL_WORKTIME_TOTAL = fuel_pump_working_time;
-	SAVE_FUEL_WORKTIME_DAY = fuel_pump_today_time;
-	SAVE_FUEL_WORKTIME_PREV = fuel_pump_prev_time;
+// void ICACHE_FLASH_ATTR save_data(uint8_t save)
+// {
+// 	// FUEL_WORK_TOTAL = fpWorkA;
+// 	// FUEL_WORK_DAY = fpWorkT;
+// 	// FUEL_WORK_PREV = fpWorkY;
+// 	// FUEL_WORK_LAST = fpWorkL;
 	
-	SAVE_FUEL_CONSUMPTION_TOTAL = i_fuel_consump;
-	SAVE_FUEL_CONSUMPTION_DAY = i_fuel_consump_today;
-	SAVE_FUEL_CONSUMPTION_PREV = i_fuel_consump_prev;
+// 	// FUEL_RATE_TOTAL = fpRateA;
+// 	// FUEL_RATE_DAY = fpRateT;
+// 	// FUEL_RATE_PREV = fpRateY;
+// 	// FUEL_RATE_LAST = fpRateL;
 
-	if (save) SAVEOPT;
-}
+// 	if (save) SAVEOPT;
+// }
 
 void ICACHE_FLASH_ATTR timerfunc(uint32_t  timersrc) 
 {
 	if ( safemode ) return;
 
-/* 	if ( timersrc % 5 == 0 )
-	{
-		if ( SAVE_FUEL_WORKTIME_PREV != fuel_pump_prev_time ) fuel_pump_prev_time = SAVE_FUEL_WORKTIME_PREV;
-		if ( SAVE_FUEL_CONSUMPTION_DAY != i_fuel_consump_today ) i_fuel_consump_today = SAVE_FUEL_CONSUMPTION_DAY;
-		if ( SAVE_FUEL_CONSUMPTION_PREV != i_fuel_consump_prev ) i_fuel_consump_prev = SAVE_FUEL_CONSUMPTION_PREV;
-		
-	} */
-	
 	// корректировка нулевых значений счетчиков
-	if ( rtc_data.fuel_consump_total == 0) rtc_data.fuel_consump_total = i_fuel_consump;
-	if ( rtc_data.fuel_consump_day == 0) rtc_data.fuel_consump_day = i_fuel_consump_today;
-	if ( rtc_data.fuel_consump_prev == 0) rtc_data.fuel_consump_prev = i_fuel_consump_prev;
+	if ( rtc_data._fpRateA == 0) rtc_data._fpRateA = fpRateA;
+	if ( rtc_data._fpRateT == 0) rtc_data._fpRateT = fpRateT;
+	if ( rtc_data._fpRateY == 0) rtc_data._fpRateY = fpRateY;
+	if ( rtc_data._fpRateL == 0) rtc_data._fpRateL = fpRateL;
 	
-	if ( rtc_data.fuel_worktime_total == 0) rtc_data.fuel_worktime_total = fuel_pump_working_time;
-	if ( rtc_data.fuel_worktime_day == 0) rtc_data.fuel_worktime_day = fuel_pump_today_time;
-	if ( rtc_data.fuel_worktime_prev == 0) rtc_data.fuel_worktime_prev = fuel_pump_prev_time;
+	if ( rtc_data._fpWorkA == 0) rtc_data._fpWorkA = fpWorkA;
+	if ( rtc_data._fpWorkT == 0) rtc_data._fpWorkT = fpWorkT;
+	if ( rtc_data._fpWorkY == 0) rtc_data._fpWorkY = fpWorkY;
+	if ( rtc_data._fpWorkL == 0) rtc_data._fpWorkL = fpWorkL;
 	
-	if ( rtc_data.on_cnt == 0) rtc_data.fuel_worktime_day = on_cnt;
-	if ( rtc_data.prev_duration == 0) rtc_data.prev_duration = prev_duration;
-	
-
+	if ( rtc_data.on_cnt == 0) rtc_data.on_cnt = on_cnt;
 	
 	if ( time_loc.hour == 1 && time_loc.min == 0 && time_loc.sec == 0 )
 	{
 		// обнулить суточные данные ночью
-		fuel_pump_prev_time = fuel_pump_today_time;
-		fuel_pump_today_time = 0;
-		//fuel_consump_today = 0;
-		i_fuel_consump_prev = i_fuel_consump_today;
-		i_fuel_consump_today = 0;
+		fpWorkY = fpWorkT;
+		fpWorkT = 0;
+		fpRateY = fpRateT;
+		fpRateT = 0;
 		on_cnt = 0;
 	} 
 	
 	static uint8_t prev_state = 0;
 	static uint32_t t_start = 0;
 	
-	//fuel_pump_state = ( cnt1i > 0 );
-	//fuel_pump_state = ( count60end[0] > 0 );
-	fuel_pump_state = ( count60end[0] > COUNTER_THRESHOLD );  // если просто > 0? то проскакивают левые импульсы
+	fpState = ( count60end[0] > COUNTER_THRESHOLD );  // если просто > 0? то проскакивают левые импульсы
 	
-	
-	if ( prev_state != fuel_pump_state )
+	if ( prev_state != fpState )
 	{
-		GPIO_ALL( FUEL_PUMP_LED_GPIO, fuel_pump_state );
-		mqtt_send_int( FUEL_PUMP, fuel_pump_state );
-		prev_state = fuel_pump_state;
+		GPIO_ALL( FUEL_PUMP_LED_GPIO, fpState );
+		mqtt_send_int( FUEL_PUMP, fpState );
+		prev_state = fpState;
 		
-		if ( fuel_pump_state ) 
+		if ( fpState ) 
 		{
 			// переключился из 0 в 1  (!!! может проскакивать импульс и поэтому cnt увеличивается на 1 и предыдущее время обнуляется, регулируется отсечкой подсчета импульсов)
 			on_cnt++;
 			t_start = millis(); // при включении начали отсчет
-			i_fuel_consump_now = 0; // обнуление текущего расхода
-			
+			fpRateN = 0; // обнуление текущего расхода	
 		} else {
 			// переключился из 1 в 0
-			prev_duration = on_duration;
-			i_fuel_consump_now = 0;
-			on_duration = 0;
+			fpWorkL = fpWorkN;
+			fpRateL = fpRateN;
+			fpRateN = 0;
+			fpWorkN = 0;
 			// сохраним в nvs
-			save_data(0);
+			//save_data(0); SAVEOPT
 		}
 	}
 	
-	if ( fuel_pump_state ) {
-		fuel_pump_working_time++;
+	if ( fpState ) {
+		fpWorkA++;
+		fpRateA += CONSUMP_L_SEC;//*100000;
 		
-		//fuel_consump += CONSUMP_L_SEC;
-		i_fuel_consump += CONSUMP_L_SEC;//*100000;
+		fpWorkT++;
+		fpRateT += CONSUMP_L_SEC;//*100000;
 		
-		fuel_pump_today_time++;
-		
-		//fuel_consump_today += CONSUMP_L_SEC;
-		i_fuel_consump_today += CONSUMP_L_SEC;//*100000;
-		
-		i_fuel_consump_now += CONSUMP_L_SEC;//*100000;
-		on_duration = millis() - t_start;	// считаем время
+		fpRateN += CONSUMP_L_SEC;//*100000;
+		fpWorkN = millis() - t_start;	// считаем время
 	}
 	
 	
 	if ( GPIO_ALL_GET( 6 ) == 1 )
 	{
-		i_fuel_consump = 0;
-		i_fuel_consump_today = 0;
-		i_fuel_consump_prev = 0;
+		fpRateA = 0;
+		fpRateT = 0;
+		fpRateY = 0;
+		fpRateL = 0;
 		
-		fuel_pump_working_time = 0;
-		fuel_pump_today_time = 0;
-		fuel_pump_prev_time = 0;
+		fpWorkA = 0;
+		fpWorkT = 0;
+		fpWorkY = 0;
+		fpWorkL = 0;
+
 		on_cnt = 0;
 		
 		GPIO_ALL(6, 0);
 		
-		save_data(1);
+		//save_data(1);
+		SAVEOPT;
 	}
 	
 	// пишем данные в rtc
-	rtc_data.fuel_consump_total = i_fuel_consump;
-	rtc_data.fuel_consump_day = i_fuel_consump_today;
-	rtc_data.fuel_consump_prev = i_fuel_consump_prev;
+	rtc_data._fpRateA = fpRateA;
+	rtc_data._fpRateT = fpRateT;
+	rtc_data._fpRateY = fpRateY;
+	rtc_data._fpRateL = fpRateL;
 	
-	rtc_data.fuel_worktime_total = fuel_pump_working_time;
-	rtc_data.fuel_worktime_day = fuel_pump_today_time;
-	rtc_data.fuel_worktime_prev = fuel_pump_prev_time;
+	rtc_data._fpWorkA = fpWorkA;
+	rtc_data._fpWorkT = fpWorkT;
+	rtc_data._fpWorkY = fpWorkY;
+	rtc_data._fpWorkL = fpWorkL;
 	
 	rtc_data.on_cnt = on_cnt;
-	rtc_data.prev_duration = prev_duration;
 	
 	uint32_t crc32 = calcCRC32( (uint8_t *)&rtc_data, sizeof(rtc_data_t));
 	rtc_data.crc32 = crc32;
 	system_rtc_mem_write(70, &rtc_data, sizeof(rtc_data_t));
 	
-	save_data(0);
+	//save_data(0);
 	
 	if ( timersrc % 3600 ) {
-		save_data(1);
+		//save_data(1);
+		SAVEOPT;
 	}
 }
 
 
 void webfunc(char *pbuf) 
 {
-	os_sprintf(HTTPBUFF, "<b>Fuel Pump:</b> %s &nbsp; <b>count:</b> %d <br>", fuel_pump_state ? "ON" : "OFF", on_cnt );
-	os_sprintf(HTTPBUFF, "<b>PrevDuration:</b> %d:%02d <br>", (prev_duration / 1000) / 60,  (prev_duration / 1000) % 60);
+	os_sprintf(HTTPBUFF, "<b>Fuel Pump:</b> %s &nbsp; <b>count:</b> %d <br>", fpState ? "ON" : "OFF", on_cnt );
 	
-	uint32_t sec = fuel_pump_working_time % 60;
-	uint32_t min = fuel_pump_working_time / 60;
-	uint32_t hour = (min / 60 % 24);
-	min = min % 60;
-
-	
-	
-		
-	//os_sprintf(HTTPBUFF, "<b>Working time (total):</b> %d days %02d:%02d:%02d<br>", day, hour, min, sec );
-	//os_sprintf(HTTPBUFF, "<b>Total consumption:</b> %d.%03d litres<br>", (int)(fuel_consump * 1000)/ 1000, (int)(fuel_consump * 1000 )% 1000);
-	
-	
-	//os_sprintf(HTTPBUFF, "<b>Working time (today):</b> %02d:%02d:%02d<br>", today_hour, today_min, today_sec );
-	//os_sprintf(HTTPBUFF, "<b>Today consumption:</b> %d.%03d litres<br>", (int)(fuel_consump_today * 1000)/ 1000, (int)(fuel_consump_today * 1000 )% 1000);
-
+	uint32_t sec, min, hour = 0;
 
 	os_sprintf(HTTPBUFF, "<table width='100%%' cellpadding='2' cellspacing='2' cols='3'>"
-							"<tr align='center'>"
-								"<th></th><th>Work time:</th><th>Consumption, L:</th>"
-							"</tr>"
+							"<tr align='center'><th></th><th>Work time:</th><th>Consumption, L:</th></tr>"
 				);
 				
 
-	os_sprintf(HTTPBUFF, 	"<tr align='center'>"
-								"<td><b>Now:</b></td><td>%02d:%02d</td><td>%d.%03d</td>"
-							"</tr>"
-							, (on_duration / 1000) / 60,  (on_duration / 1000) % 60
-							, i_fuel_consump_now / 100000, (i_fuel_consump_now % 100000) / 100
+	os_sprintf(HTTPBUFF, 	"<tr align='center'><td><b>Now:</b></td><td>%02d:%02d</td><td>%d.%03d</td></tr>"
+							, (fpWorkN / 1000) / 60,  (fpWorkN / 1000) % 60
+							, fpRateN / 100000, (fpRateN % 100000) / 100
 	);				
 
-	uint32_t _sec = fuel_pump_today_time % 60;
-	uint32_t _min = fuel_pump_today_time / 60;
-	uint32_t _hour = _min / 60;
-	_min = _min % 60;
+	os_sprintf(HTTPBUFF, 	"<tr align='center'><td><b>Last:</b></td><td>%02d:%02d</td><td>%d.%03d</td></tr>"
+							, (fpWorkL / 1000) / 60,  (fpWorkL / 1000) % 60
+							, fpRateL / 100000, (fpRateL % 100000) / 100
+	);
 
-	os_sprintf(HTTPBUFF, 	"<tr align='center'>"
-								"<td><b>Today:</b></td><td>%02d:%02d:%02d</td><td>%d.%03d</td>"
-							"</tr>"
-							, _hour, _min, _sec
-							, i_fuel_consump_today / 100000, (i_fuel_consump_today % 100000) / 100
+	sec = fpWorkT % 60;
+	min = fpWorkT / 60;
+	hour = min / 60;
+	min = min % 60;
+	os_sprintf(HTTPBUFF, 	"<tr align='center'><td><b>Today:</b></td><td>%02d:%02d:%02d</td><td>%d.%03d</td></tr>"
+							, hour, min, sec
+							, fpRateT / 100000, (fpRateT % 100000) / 100
 	);
 
 
-	_sec = fuel_pump_prev_time % 60;
-	_min = fuel_pump_prev_time / 60;
-	_hour = _min / 60;
-	_min = _min % 60;
-
-	os_sprintf(HTTPBUFF, 	"<tr align='center'>"
-								"<td><b>Yesterday:</b></td><td>%02d:%02d:%02d</td><td>%d.%03d</td>"
-							"</tr>"
-							, _hour, _min, _sec
-							, i_fuel_consump_prev / 100000, (i_fuel_consump_prev % 100000) / 100
+	sec = fpWorkY % 60;
+	min = fpWorkY / 60;
+	hour = min / 60;
+	min = min % 60;
+	os_sprintf(HTTPBUFF, 	"<tr align='center'><td><b>Yesterday:</b></td><td>%02d:%02d:%02d</td><td>%d.%03d</td></tr>"
+							, hour, min, sec
+							, fpRateY / 100000, (fpRateY % 100000) / 100
 	);
 	
-	os_sprintf(HTTPBUFF, 	"<tr align='center'>"
-								"<td><b>Total:</b></td><td>%02d:%02d:%02d</td><td>%d.%03d</td>"
-							"</tr>"
+	sec = fpWorkA % 60;
+	min = fpWorkA / 60;
+	hour = (min / 60 % 24);
+	min = min % 60;
+	os_sprintf(HTTPBUFF, 	"<tr align='center'><td><b>Total:</b></td><td>%02d:%02d:%02d</td><td>%d.%03d</td></tr>"
 							, hour, min, sec
-							, i_fuel_consump / 100000, (i_fuel_consump % 100000) / 100
+							, fpRateA / 100000, (fpRateA % 100000) / 100
 	);	
 	
 	os_sprintf(HTTPBUFF, 	"</table>");	
+	os_sprintf(HTTPBUFF, 	"<br><small>ver.%s</small>", FW_VER);	
 	
 }
